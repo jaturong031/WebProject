b93598a2d53a512b97a823aea455ceef930e40f2
sadtawut <5710121078@email.psu.ac.th>
2014-11-02 12:38:37 +0700

    addstyle sadtawut

---
+++ include/jacript.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+ $(function() {
+                $("#slideshow1").slideshow_buttontitle({
+                    time_interval: '3000',
+                    window_background_color: "#FFF",
+                    window_padding: '0',
+                    window_width: '1024',
+                    window_height: '300',
+                    border_size: '0',
+                    border_color: 'black',
+                    title_text_color: 'white',
+                    title_background_color: '#EC1939',
+                    button_color: '#84C5D2',
+                    button_current_color: '#2C7E8F', 
+                    button_size: '15'
+                });
+				$("#slideshow2").slideshow_buttontitle({
+                    time_interval: '3000',
+                    window_background_color: "#FFF",
+                    window_padding: '2',
+                    window_width: '393',
+                    window_height: 'auto',
+                    border_size: '0',
+                    border_color: '#9C0',
+                    title_text_color: '#000',
+                    title_background_color: '#9C0',
+                    button_color: '#9C0',
+                    button_current_color: '#9F0',
+                    button_size: '10'
+                });
+            });
